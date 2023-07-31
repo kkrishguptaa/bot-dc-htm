1c17c7d4806c0a1c8b6d423b1e166c560a43f4f0
Krish Gupta <krishstwt@gmail.com>
2023-07-31 13:43:08 +0000

    feat: express server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import './lib/setup';
 import { LogLevel, SapphireClient } from '@sapphire/framework';
+import { cyan } from 'colorette';
 import { GatewayIntentBits } from 'discord.js';
+import express from 'express'
 
 const client = new SapphireClient({
 	defaultPrefix: '!',
@@ -12,11 +14,21 @@ const client = new SapphireClient({
 	loadMessageCommandListeners: true
 });
 
+const app = express()
+
 const main = async () => {
 	try {
 		client.logger.info('Logging in');
 		await client.login();
 		client.logger.info('Logged in');
+
+    app.listen(3000, () => {
+      client.logger.info(`Listening on port ${cyan(3000)}, http://localhost:3000`)
+    })
+
+    app.get('/', (_, res) => {
+      res.json(client.toJSON())
+    })
 	} catch (error) {
 		client.logger.fatal(error);
 		client.destroy();
