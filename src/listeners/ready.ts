b9b2391953f9fcc1ff5c682a76069d6bf63d4b4f
Krish Gupta <xkrishguptaa@Krishs-MacBook-Air.local>
2023-10-21 01:15:03 +0530

    add registrations status

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import { Listener } from '@sapphire/framework'
 import { bgMagenta, bold } from 'colorette'
+import { ActivityType } from 'discord.js'
 
 export class UserEvent extends Listener {
 
@@ -12,6 +13,12 @@ export class UserEvent extends Listener {
   }
 
   public run () {
+    this.container.client.user?.setStatus('idle')
+    this.container.client.user?.setActivity({
+      name: 'HTM4 Registrations Skyrocket',
+      type: ActivityType.Watching,
+      url: 'https://hackthemountain.tech'
+    })
     this.container.logger.info(bgMagenta(bold('Discord Service')))
     this.container.logger.info(bgMagenta(`Power Level: ${bold('OVER 9000')}`))
   }
